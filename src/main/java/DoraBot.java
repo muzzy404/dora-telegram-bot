8fda652929404dc9e41affa6d3f513a9422f2031
Daria <muzzy404@gmail.com>
2017-07-01 19:25:09 +0300

    echo bot works

--- src/main/java/DoraBot.java
+++ src/main/java/DoraBot.java
@@ -1,21 +1,44 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 /**
  * Created by Daria on 01.07.2017.
  */
 public class DoraBot extends TelegramLongPollingBot {
+
+    private static final String token = "428910742:AAExeR2y1tZqZ50YR4q-jenyUE6XIjvexIg";
+
     public void onUpdateReceived(Update update) {
-        //TODO
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            SendMessage answer = new SendMessage()
+                    .setChatId(chatId)
+                    .setText(messageText);
+
+            try {
+
+                sendMessage(answer);
+
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+        } // end if
+
     }
 
     public String getBotUsername() {
-        //TODO
-        return null;
+        // return bot name
+        return getBotToken();
     }
 
     public String getBotToken() {
-        //TODO
-        return null;
+        return token;
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by Daria on 01.07.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // initialize Api Context
+        ApiContextInitializer.init();
+        // instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // register our bot
+        try {
+            botsApi.registerBot(new DoraBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
