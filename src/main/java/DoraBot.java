9be73af5ebb0291ff16e42e506e27d45a96ebf49
Daria <muzzy404@gmail.com>
2017-07-01 18:26:50 +0300

    Initial commit

---
+++ src/main/java/DoraBot.java
@@ -0,0 +1,21 @@
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+/**
+ * Created by Daria on 01.07.2017.
+ */
+public class DoraBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        //TODO
+    }
+
+    public String getBotUsername() {
+        //TODO
+        return null;
+    }
+
+    public String getBotToken() {
+        //TODO
+        return null;
+    }
+}
